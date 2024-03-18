adcd8590f34f2ab627ed363cd70891382a7af480
Sirinya <sry.ongkh@gmail.com>
2024-03-19 00:43:38 +0700

    Add destination content

--- app/destination/layout.tsx
+++ app/destination/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Tab from "@/components/tab/tab";
+import DestinyContent from "@/components/destinyContent/destinyContent";
 export default function DestinationLayout({
   children,
 }: Readonly<{
@@ -11,28 +12,7 @@ export default function DestinationLayout({
         {children}
         <div className="w-2/5 min-h-[445px] h-full flex flex-col justify-center">
           <Tab />
-          <h2 className="font-bellefair text-h2 uppercase my-9">Moon</h2>
-          <p className="leading-8 text-purple text-bodytxt pb-7">
-            See our planet as you’ve never seen it before. A perfect relaxing
-            trip away to help regain perspective and come back refreshed. While
-            you’re there, take in some history by visiting the Luna 2 and Apollo
-            11 landing sites.
-          </p>
-          <hr className="my-7" />
-          <div className="w-full flex">
-            <div className="w-1/2 uppercase">
-              <p className="label text-purple text-sm tracking-subheading mb-3">AVG. DISTANCE</p>
-              <p className="text-subh1 font-bellefair">
-                384,400 <span>km</span>
-              </p>
-            </div>
-            <div className="w-1/2 uppercase">
-              <p className="label text-purple text-sm tracking-subheading mb-3">Est. travel time</p>
-              <p className="text-subh1 font-bellefair">
-                3 <span>days</span>
-              </p>
-            </div>
-          </div>
+          <DestinyContent />
         </div>
       </div>
     </>
---
+++ components/destinyContent/destinyContent.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const DestinyContent = () => {
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bellefair text-h2 uppercase my-9">Moon</h2>
+      <p className="leading-8 text-purple text-bodytxt pb-7">
+        See our planet as you’ve never seen it before. A perfect relaxing trip
+        away to help regain perspective and come back refreshed. While you’re
+        there, take in some history by visiting the Luna 2 and Apollo 11 landing
+        sites.
+      </p>
+      <hr className="my-7" />
+      <div className="w-full flex">
+        <div className="w-1/2 uppercase">
+          <p className="label text-purple text-sm tracking-subheading mb-3">
+            AVG. DISTANCE
+          </p>
+          <p className="text-subh1 font-bellefair">
+            384,400 <span>km</span>
+          </p>
+        </div>
+        <div className="w-1/2 uppercase">
+          <p className="label text-purple text-sm tracking-subheading mb-3">
+            Est. travel time
+          </p>
+          <p className="text-subh1 font-bellefair">
+            3 <span>days</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DestinyContent;
