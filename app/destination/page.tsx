46032bfdd636af6142962188543531818747e8c3
Sirinya <sry.ongkh@gmail.com>
2024-03-19 21:33:34 +0700

    (Edit) useSate

--- app/destination/page.tsx
+++ app/destination/page.tsx
@@ -6,17 +6,19 @@ import gsap from "gsap";
 import Tab from "@/components/tab/tab";
 import DestinationContent from "@/components/destinyContent/destinyContent";
 import { motion } from "framer-motion";
-export default function destinationPage() {
+
+export default function DestinationPage() {
   const [selectedPlanet, setSelectedPlanet] = React.useState(
     destinations[0].planet
   );
   const selectedDestination = destinations.find(
     (destination) => destination.planet === selectedPlanet
   );
+
   React.useEffect(() => {
     const starContainer = document.querySelector(".star-container");
     const star = document.querySelector(".destination");
-    if (star) {
+    if (starContainer && star) {
       gsap
         .fromTo(
           starContainer,
@@ -26,29 +28,30 @@ export default function destinationPage() {
         .then(() => {
           gsap.to(star, {
             rotation: 360,
-            duration: 32,
-            ease: "ease",
+            duration: 28,
+            ease: "none",
             repeat: -1,
           });
         });
     }
-  }, []);
+  }, [selectedDestination]);
   return (
     <>
       <div className="w-full h-[80%] mt-[auto] px-[11.458vw] pb-32 flex text-white relative z-[0]">
+        {/* Content */}
         <div className="w-3/5">
           <h5 className="text-h5 tracking-h5 uppercase">
             <span>01</span> Pick your destination
           </h5>
-          <div className="relative top-0 left-0 w-full h-full flex justify-center items-center">
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5 }}
-              className="star-container opacity-0 relative w-fit h-fit rounded-full overflow-hidden bg-primary shadow-star shadow-white"
-            >
-              <div className="absolute -bottom-[26rem] w-[761px] h-[761px] bg-primary rounded-full z-[1] blur-2xl"></div>
-              {selectedDestination ? (
+          <div className="w-full h-full flex justify-center items-center">
+            {selectedDestination ? (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5 }}
+                className="star-container opacity-0 relative w-fit h-fit rounded-full overflow-hidden bg-primary shadow-star shadow-white m-auto"
+              >
+                <div className="absolute -bottom-[26rem] w-[761px] h-[761px] bg-primary rounded-full z-[1] blur-2xl"></div>
                 <Image
                   src={selectedDestination.img}
                   alt={selectedDestination.planet}
@@ -57,13 +60,13 @@ export default function destinationPage() {
                   className="destination w-auto m-auto relative z-[0]"
                   priority
                 />
-              ) : (
-                <p>Destination not found</p>
-              )}
-            </motion.div>
+              </motion.div>
+            ) : (
+              <p>Destination not found</p>
+            )}
           </div>
         </div>
-        <div className="w-2/5 min-h-[445px] h-full flex flex-col">
+        <div className="w-2/5 min-h-[445px] h-full flex flex-col justify-center">
           <Tab destinations={destinations} onPlanetSelect={setSelectedPlanet} />
           {selectedDestination && (
             <DestinationContent destination={selectedDestination} />
